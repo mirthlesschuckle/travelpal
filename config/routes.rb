d2bdc8ab82c690183db7a531b97cf2266d5ce2fa
Jianhao Yu <yujianhao1314@gmail.com>
2023-03-21 01:14:15 +0000

    Created profile route, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Defines the route for the user profile page (users controller)
+  get "/profile", to: "users#profile", as: :profile
 end
