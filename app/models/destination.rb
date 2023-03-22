e14c195e97a8f7400f15b1e5611e9dec998a6906
Mikitanjie <121625223+Mikitanjie@users.noreply.github.com>
2023-03-22 16:40:40 +0100

    set-markersAndsearch

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,9 +1,8 @@
 class Destination < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   acts_as_favoritable
 
   has_many :activities, dependent: :destroy
   has_many :trips, dependent: :destroy
-
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
 end
