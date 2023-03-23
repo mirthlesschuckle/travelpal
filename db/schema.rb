8c0b7c2ebf188705f470efc7a163be86cb3ca2ee
mirthlesschuckle <123471987+mirthlesschuckle@users.noreply.github.com>
2023-03-23 15:57:53 +0000

    smaller seed with images and extra styling on homepage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_21_214734) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_23_120126) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_214734) do
     t.float "longitude"
     t.string "country"
     t.string "area"
+    t.string "image_path"
   end
 
   create_table "favorites", force: :cascade do |t|
