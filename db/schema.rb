bb49500e39cd78c829dc66502f5a69f1577a99fb
mirthlesschuckle <123471987+mirthlesschuckle@users.noreply.github.com>
2023-03-24 12:44:28 +0000

    added basic chat function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_23_120126) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_24_113104) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_23_120126) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["destination_id"], name: "index_activities_on_destination_id"
+  end
+
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "destinations", force: :cascade do |t|
@@ -65,6 +71,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_23_120126) do
     t.index ["user_id"], name: "index_matches_on_user_id"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "reviews", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "content"
@@ -103,6 +119,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_23_120126) do
   add_foreign_key "activities", "destinations"
   add_foreign_key "matches", "trips"
   add_foreign_key "matches", "users"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "reviews", "users"
   add_foreign_key "trips", "destinations"
   add_foreign_key "trips", "users"
